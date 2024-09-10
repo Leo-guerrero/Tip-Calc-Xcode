5327abb734cd24901c4fbc56c10f282e10d97e59
Leo-guerrero <sja.leo.guerrero@gmail.com>
2024-09-10 12:17:25 -0500

    Add files via upload

--- Tip Calculator/Tip Calculator/ViewController.swift
+++ Tip Calculator/Tip Calculator/ViewController.swift
@@ -13,7 +13,58 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
+    @IBOutlet weak var txtBillAmount: UITextField!
+    
+    
+    
+    
+    @IBOutlet weak var segTipPercent: UISegmentedControl!
+    
+    
 
-
+    @IBAction func btnCalculateTip(_ sender: UIButton) {
+        
+        let userInput = Float(txtBillAmount.text!)
+        
+        if (userInput == nil){
+            
+            let alertController = UIAlertController(title: "Incorrect Bill Amount", message: "Wrong thing pal", preferredStyle: UIAlertController.Style.alert)
+            
+            alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+            
+            present(alertController, animated: true, completion: nil)
+            return
+        }
+        
+        let index : Int = segTipPercent.selectedSegmentIndex
+        
+        var tiprate : Float = 0.18
+        
+        
+        if index == 0 {
+            tiprate = 0.18
+        }
+        else if index == 1{
+            tiprate = 0.20
+        }
+        else{
+            tiprate = 0.24
+        }
+        
+        let tip = userInput! * tiprate
+        
+        
+        let display = String(format: "%.2f", tip)
+        
+        tipAmount.text = "$" + display
+    }
+    
+    
+    
+    
+    @IBOutlet weak var tipAmount: UILabel!
+    
 }
 
